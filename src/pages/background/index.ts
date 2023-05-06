bda39276da9548b0e78058f0c5bf6ee127b3d75b
remusris <remus.risnoveanu@gmail.com>
2023-05-06 15:41:44 -0700

    chrome.storage.local change + methods for accessing it's keys

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -57,39 +57,60 @@ const chromeStorageKeys = {
   supabaseExpiration: "supabaseExpiration",
 };
 
+async function getKeyFromStorage(key) {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get(key, (result) => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        resolve(result[key]);
+      }
+    });
+  });
+}
+
+async function setKeyInStorage(
+  keyValuePairs: Record<string, any>
+): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    chrome.storage.local.set(keyValuePairs, () => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+async function removeKeysFromStorage(keys: string[]): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    chrome.storage.local.remove(keys, () => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
 async function handleMessage(
   { action, value }: Message,
   response: ResponseCallback
 ) {
-  /* const getSupabaseClient = async (): Promise<SupabaseClient> => {
-    return new Promise((resolve) => {
-      chrome.storage.sync.get(
-        chromeStorageKeys.supabaseAccessToken,
-        (result) => {
-          const accessToken = result[chromeStorageKeys.supabaseAccessToken];
-          const supabase = createClient(SUPABASE_URL, accessToken);
-          resolve(supabase);
-        }
-      );
-    });
-  }; */
-
   if (action === "signin") {
     console.log("requesting auth");
     const { data, error } = await supabase.auth.signInWithPassword(value);
     console.log("token expiration", data.session.expires_at);
     if (data) {
-      chrome.storage.sync.set(
-        {
-          [chromeStorageKeys.supabaseAccessToken]: data.session.access_token,
-          [chromeStorageKeys.supabaseRefreshToken]: data.session.refresh_token,
-          [chromeStorageKeys.supabaseUserData]: data.user,
-          [chromeStorageKeys.supabaseExpiration]: data.session.expires_at,
-        },
-        () => {
-          console.log("User data stored in chrome.storage.sync");
-        }
-      );
+      await setKeyInStorage({
+        [chromeStorageKeys.supabaseAccessToken]: data.session.access_token,
+        [chromeStorageKeys.supabaseRefreshToken]: data.session.refresh_token,
+        [chromeStorageKeys.supabaseUserData]: data.user,
+        [chromeStorageKeys.supabaseExpiration]: data.session.expires_at,
+      });
+      console.log("User data stored in chrome.storage.sync");
       response({ data, error });
     } else {
       response({ data: null, error: "No active session" });
@@ -97,17 +118,13 @@ async function handleMessage(
   } else if (action === "signup") {
     const { data, error } = await supabase.auth.signUp(value);
     if (data) {
-      chrome.storage.sync.set(
-        {
-          [chromeStorageKeys.supabaseAccessToken]: data.session.access_token,
-          [chromeStorageKeys.supabaseRefreshToken]: data.session.refresh_token,
-          [chromeStorageKeys.supabaseUserData]: data.user,
-          [chromeStorageKeys.supabaseExpiration]: data.session.expires_at,
-        },
-        () => {
-          console.log("User data stored in chrome.storage.sync");
-        }
-      );
+      await setKeyInStorage({
+        [chromeStorageKeys.supabaseAccessToken]: data.session.access_token,
+        [chromeStorageKeys.supabaseRefreshToken]: data.session.refresh_token,
+        [chromeStorageKeys.supabaseUserData]: data.user,
+        [chromeStorageKeys.supabaseExpiration]: data.session.expires_at,
+      });
+      console.log("User data stored in chrome.storage.sync");
       response({ message: "Successfully signed up!", data: data });
     } else {
       response({ data: null, error: error?.message || "Signup failed" });
@@ -115,57 +132,42 @@ async function handleMessage(
   } else if (action === "signout") {
     const { error } = await supabase.auth.signOut();
     if (!error) {
-      // Clear user data from chrome.storage.sync
-      chrome.storage.sync.remove(
-        [
-          chromeStorageKeys.supabaseAccessToken,
-          chromeStorageKeys.supabaseRefreshToken,
-          chromeStorageKeys.supabaseUserData,
-          chromeStorageKeys.supabaseExpiration,
-        ],
-        () => {
-          console.log("User data removed from chrome.storage.sync");
-        }
-      );
+      await removeKeysFromStorage([
+        chromeStorageKeys.supabaseAccessToken,
+        chromeStorageKeys.supabaseRefreshToken,
+        chromeStorageKeys.supabaseUserData,
+        chromeStorageKeys.supabaseExpiration,
+      ]);
+      console.log("User data removed from chrome.storage.sync");
       response({ message: "Successfully signed out!" });
     } else {
       response({ error: error?.message || "Signout failed" });
     }
   } else if (action === "refresh") {
-    chrome.storage.sync.get(
-      chromeStorageKeys.supabaseRefreshToken,
-      async (result) => {
-        const refreshToken = result[chromeStorageKeys.supabaseRefreshToken];
-        console.log("refresh token to input", refreshToken);
-        if (refreshToken) {
-          const { data, error } = await supabase.auth.refreshSession({
-            refresh_token: refreshToken,
-          });
-          console.log("data from refresh", data);
-          if (data) {
-            console.log("data fetch", data.session.user);
-            chrome.storage.sync.set(
-              {
-                [chromeStorageKeys.supabaseAccessToken]:
-                  data.session.access_token,
-                [chromeStorageKeys.supabaseRefreshToken]:
-                  data.session.refresh_token,
-                [chromeStorageKeys.supabaseUserData]: data.user,
-                [chromeStorageKeys.supabaseExpiration]: data.session.expires_at,
-              },
-              () => {
-                console.log("User data refreshed in chrome.storage.sync");
-              }
-            );
-            response({ data: data });
-          } else {
-            response({ data: null, error: "Refresh failed" });
-          }
-        } else {
-          response({ data: null, error: "No refresh token available" });
-        }
+    const refreshToken = (await getKeyFromStorage(
+      chromeStorageKeys.supabaseRefreshToken
+    )) as string;
+    if (refreshToken) {
+      const { data, error } = await supabase.auth.refreshSession({
+        refresh_token: refreshToken,
+      });
+
+      if (data) {
+        await setKeyInStorage({
+          [chromeStorageKeys.supabaseAccessToken]: data.session.access_token,
+          [chromeStorageKeys.supabaseRefreshToken]: data.session.refresh_token,
+          [chromeStorageKeys.supabaseUserData]: data.user,
+          [chromeStorageKeys.supabaseExpiration]: data.session.expires_at,
+        });
+
+        console.log("User data refreshed in chrome.storage.sync");
+        response({ data: data });
+      } else {
+        response({ data: null, error: "Refresh failed" });
       }
-    );
+    } else {
+      response({ data: null, error: "No refresh token available" });
+    }
   } else if (action === "fetchSmoothies") {
     const { data, error } = await supabase.from("smoothies").select();
 
@@ -227,50 +229,36 @@ chrome.runtime.onMessage.addListener((msg, sender, response) => {
 chrome.history.onVisited.addListener(async function (historyItem) {
   console.log("historyItem inside");
 
-  const SUPABASE_URL =
-    "https://xmduxigoopgpitwcmbud.supabase.co/rest/v1/url_uploader";
-
   try {
-    // Get the supabaseAccessToken from chrome.storage.sync
-    chrome.storage.sync.get(
-      chromeStorageKeys.supabaseAccessToken,
-      async (result) => {
-        const supabaseAccessToken =
-          result[chromeStorageKeys.supabaseAccessToken];
-        console.log("supabaseAccessToken", supabaseAccessToken);
-        console.log("historyItem inside 2");
-
-        if (!supabaseAccessToken) {
-          console.error("No Supabase access token found");
-          return;
-        }
-
-        try {
-          const response = await fetch(SUPABASE_URL, {
-            method: "POST",
-            headers: {
-              apikey: SUPABASE_KEY,
-              Authorization: `Bearer ${supabaseAccessToken}`,
-              "Content-Type": "application/json",
-              Prefer: "return=minimal",
-            },
-            body: JSON.stringify({ url: historyItem.url }),
-          });
-
-          if (!response.ok) {
-            const errorResponse = await response.json();
-            console.error("Error response:", errorResponse);
-            throw new Error(`HTTP error! status: ${response.status}`);
-          }
-
-          console.log("URL uploaded successfully:", historyItem.url);
-        } catch (error) {
-          console.error("Error uploading URL:", error.message);
-        }
-      }
+    const supabaseAccessToken = await getKeyFromStorage(
+      chromeStorageKeys.supabaseAccessToken
     );
+
+    if (!supabaseAccessToken) {
+      console.error("No Supabase access token found");
+      return;
+    }
+
+    const response = await fetch(SUPABASE_URL, {
+      method: "POST",
+      headers: {
+        apikey: SUPABASE_KEY,
+        Authorization: `Bearer ${supabaseAccessToken}`,
+        "Content-Type": "application/json",
+        Prefer: "return=minimal",
+      },
+      body: JSON.stringify({ url: historyItem.url }),
+    });
+
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      console.error("Error response:", errorResponse);
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    console.log("URL uploaded successfully:", historyItem.url);
   } catch (error) {
-    console.error("Error fetching Supabase access token:", error.message);
+    console.error("Error uploading URL:", error.message);
   }
 });
 
